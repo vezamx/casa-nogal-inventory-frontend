ac0f96c25a352811fb9ed68d1b6350faeeffa56a
C3M3Z4C4 <cmeza@mezadigital.com>
2024-08-05 18:49:55 -0600

    feat/mergeOrders Agregar ordenes llamando a componente desde boton

--- src/app/components/sideBars/SelectedOrderSection.tsx
+++ src/app/components/sideBars/SelectedOrderSection.tsx
@@ -1,26 +1,30 @@
-import { Grid } from "@chakra-ui/react";
-import React, { Dispatch, SetStateAction } from "react";
-import MenuIconButton from "../Buttons/MenuIconButtons";
+import { Grid } from '@chakra-ui/react';
+import React, { Dispatch, SetStateAction } from 'react';
+import MenuIconButton from '../Buttons/MenuIconButtons';
 
 interface SelectedOrderSectionProps {
-  setSelectedOrder: Dispatch<SetStateAction<string>>;
+    setSelectedOrder: Dispatch<SetStateAction<string>>;
 }
 const SelectedOrderSection = () => {
-  return (
-    <Grid
-      as={"section"}
-      w={"50%"}
-      templateColumns={"repeat(4, 1fr)"}
-      templateRows={"repeat(12, 1fr)"}
-      mt={"10rem"}
-    >
-      <MenuIconButton label="Pagar" image="/Pagar.svg" />
-      <MenuIconButton label="Dividir Cuenta" image="/dividirCuenta.svg" />
-      <MenuIconButton label="Añadir Producto" image="/addProduct.svg" />
-      <MenuIconButton label="Eliminar Producto" image="/eliminarProducto.svg" />
-      <MenuIconButton label="Menú" image="/Menu.svg" size="lg" />
-    </Grid>
-  );
+    return (
+        <Grid
+            as={'section'}
+            w={'50%'}
+            templateColumns={'repeat(4, 2fr)'}
+            templateRows={'repeat(12, 1fr)'}
+            mt={'10rem'}
+        >
+            <MenuIconButton label="Pagar" image="/Pagar.svg" />
+            <MenuIconButton label="Menú" image="/Menu.svg" />
+            <MenuIconButton label="Dividir Cuenta" image="/dividirCuenta.svg" />
+            <MenuIconButton label="Unir Cuentas" image="/agregarCuenta.svg" />
+            <MenuIconButton label="Añadir Producto" image="/addProduct.svg" />
+            <MenuIconButton
+                label="Eliminar Producto"
+                image="/eliminarProducto.svg"
+            />
+        </Grid>
+    );
 };
 
 export default SelectedOrderSection;
